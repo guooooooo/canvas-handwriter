f2251ed0550e099b8220ec99fdf64eee239dbe2c
guolinhao <gavin141113@163.com>
2019-06-22 23:14:11 +0800

    根据速度调整笔画粗细

--- handwrite.js
+++ handwrite.js
@@ -2,6 +2,8 @@ var canvasWidth = 800;
 var canvasHeight = canvasWidth;
 var isMouseDown = false;
 var lastLoc = {x: 0, y: 0};
+var lastTimestamp = 0;
+var lastLineWidth = -1;
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
@@ -15,6 +17,7 @@ canvas.onmousedown = function(e) {
     e.preventDefault();
     isMouseDown = true;
     lastLoc = windowToCanvas(e.clientX, e.clientY);
+    lastTimestamp = new Date().getTime();
 }
 canvas.onmouseup = function(e) {
     e.preventDefault();
@@ -28,14 +31,49 @@ canvas.onmousemove = function(e) {
     e.preventDefault();
     if (isMouseDown) {
         var curLoc = windowToCanvas(e.clientX, e.clientY);
+        var curTimestamp = new Date().getTime();
+        var s = calcDistance(curLoc, lastLoc);
+        var t = curTimestamp - lastTimestamp;
+
+        var lineWidth = calcLineWidth(t, s);
         // draw
         context.beginPath();
+
         context.moveTo(lastLoc.x, lastLoc.y);
         context.lineTo(curLoc.x, curLoc.y);
+
+        context.lineWidth = lineWidth;
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+
         context.stroke();
 
         lastLoc = curLoc;
+        lastTimestamp = curTimestamp;
+        lastLineWidth = lineWidth;
     }
+}
+
+function calcDistance(loc1, loc2) {
+    return Math.sqrt((loc1.x - loc2.x)*(loc1.x - loc2.x) + (loc1.y - loc2.y)*(loc1.y - loc2.y));
+}
+
+function calcLineWidth(t, s) {
+    var v = s / t;
+    var result;
+    if (v < 0.1) {
+        result = 30;
+    } else if(v >= 10) {
+        result = 1;
+    } else {
+        result = 30 - (v-0.1)/(10-0.1)*(30-1);
+    }
+
+    if (lastLineWidth === -1) {
+        return result;
+    }
+
+    return lastLineWidth*5/6 + result*1/6;
 }
 
 function windowToCanvas(x, y) {
